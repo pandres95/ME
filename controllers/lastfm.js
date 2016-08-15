9599875032593e9315b2e3e20d9635d4b58bd5b5
Pablo Andrés Dorado Suárez <im@pandres95.me>
2016-08-15 03:56:06 -0500

    [Controllers,Routes:Lastfm] Update frontend

--- controllers/lastfm.js
+++ controllers/lastfm.js
@@ -2,8 +2,13 @@
 
 module.exports = function (app) {
     const promise = new app.views.Json().promise;
+    const Lastfm  = app.dao.Lastfm;
 
-    this.nowPlaying = function (req, res, next) {
-        promise(Promise.resolve(null), res, next);
+    this.recent = function (req, res, next) {
+        promise(new Lastfm().recent(), res, next);
+    };
+
+    this.latest = function (req, res, next) {
+        promise(new Lastfm().latest(), res, next);
     };
 };
